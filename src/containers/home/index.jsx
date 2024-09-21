67a1d0e6807018a74bf0d07458ae710c436d0133
MavithaShehar <mavithashehar@gmail.com>
2024-09-21 14:21:49 +0530

    change home strackcher & styles sheet

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Animate } from "react-simple-animate";
 import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
 import './styles.scss';
 import logo from "../../assets/imgs/cover_img.jpeg";
-
+import Aos from "aos";
+import 'aos/dist/aos.css';
 
 const Home = () => {
     const navigate = useNavigate();
+
+    useEffect(() => {
+        Aos.init({ duration: 1500 });
+    }, []);
 
     const handelNavigateToContactMe = () => {
         navigate('/contact');
@@ -15,37 +20,54 @@ const Home = () => {
 
     return (
         <section id="home" className="home">
-
             <div className="home__resose-wrapper">
-                <a href="https://github.com/MavithaShehar" target="_blank" rel="noopener noreferrer">
-                    <FaFacebook />
-                </a>
-                <a href="https://github.com/MavithaShehar" target="_blank" rel="noopener noreferrer">
-                    <FaGithub />
-                </a>
-                <a href="https://www.linkedin.com/in/sheharmavitha/" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin />
-                </a>
-            </div>
 
-            <div className="home__my-image">
-                <img style={{width:400 ,height:440}} src={logo} alt="My Logo" /> 
-            </div>
-
-            <div className="home__text-wrapper">
-                <h1>Hello! I'm <br /> <span>Shehar Mavitha</span> <br /> Full Stack Developer</h1>
-            </div>
-
-            <Animate
-                play
-                duration={1.5}
-                start={{ transform: "translateY(550px)" }}
-                end={{ transform: "translateX(0px)" }}
-            >
-                <div className="home__contact-me">
-                    <button onClick={handelNavigateToContactMe}>Hire Me</button>
+                <div data-aos="zoom-out"  className="home__resose-wrapper__my-image">
+                    <img src={logo} alt="My Logo" />
                 </div>
-            </Animate>
+
+                <div className="home__resose-wrapper__text-wrapper">
+                    <div className="home__resose-wrapper__text-wrapper__text">
+                        <h1>Hello! I'm <br /> <span>Shehar Mavitha</span> <br /> Full Stack Developer</h1>
+                    </div>
+
+                    <div  data-aos="zoom-in" className="home__resose-wrapper__contact">
+                        <a href="https://github.com/MavithaShehar" target="_blank" rel="noopener noreferrer">
+                            <FaFacebook />
+                        </a>
+                        <a href="https://github.com/MavithaShehar" target="_blank" rel="noopener noreferrer">
+                            <FaGithub />
+                        </a>
+                        <a href="https://www.linkedin.com/in/sheharmavitha/" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin />
+                        </a>
+                    </div>
+
+                    <div className="home__resose-wrapper__text-wrapper__btn">
+                        <Animate
+                            play
+                            duration={1.5}
+                            start={{ transform: "translateY(550px)" }}
+                            end={{ transform: "translateX(0px)" }}
+                        >
+                            <div className="home__contact-me">
+                                <button onClick={handelNavigateToContactMe}>Download CV</button>
+                            </div>
+                        </Animate>
+
+                        <Animate
+                            play
+                            duration={1.5}
+                            start={{ transform: "translateY(550px)" }}
+                            end={{ transform: "translateX(0px)" }}
+                        >
+                            <div className="home__contact-me">
+                                <button onClick={handelNavigateToContactMe}>Hire Me</button>
+                            </div>
+                        </Animate>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
