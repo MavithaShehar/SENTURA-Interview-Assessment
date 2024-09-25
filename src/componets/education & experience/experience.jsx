b4b350ad47545a9c60d1e9bb391bbe9261a21881
MavithaShehar <mavithashehar@gmail.com>
2024-09-25 22:26:36 +0530

    change my details of aboutMe section

--- src/componets/education & experience/experience.jsx
+++ src/componets/education & experience/experience.jsx
@@ -13,27 +13,27 @@ function experience() {
 
         <div className='education-container__info__list'>
           <h2>Front-End Developer</h2>
-          <h4>A Front-End Developer uses React, HTML, JavaScript, CSS, SCSS, and Bootstrap to build responsive, interactive web interfaces. They ensure seamless user experiences with modern styling and efficient, accessible code across devices.</h4>
+          <h4>A Front-End Developer uses React, HTML, JavaScript, CSS, SCSS, and Bootstrap to create responsive, interactive websites with modern designs and efficient code for all devices.</h4>
         </div>
 
         <div className='education-container__info__list'>
           <h2>Back-End Developer</h2>
-          <h4>A Back-End Developer utilizes Express.js, Spring Boot, and Node.js to create robust server-side applications. They manage server logic, integrate databases like MySQL and MongoDB, and ensure secure, scalable back-end solutions.</h4>
+          <h4>A Back-End Developer uses Express.js, Spring Boot, and Node.js to build strong server-side apps. They handle server logic and ensure secure, scalable systems.</h4>
         </div>
 
         <div className='education-container__info__list'>
           <h2>Databases</h2>
-          <h4>Proficient in managing and querying databases such as MySQL and MongoDB. They design and implement data models, optimize queries, and ensure data integrity for efficient application performance.</h4>
+          <h4>Skilled in managing databases like MySQL and MongoDB, designing models, optimizing queries, and ensuring data accuracy for better performance.</h4>
         </div>
 
         <div className='education-container__info__list'>
           <h2>Version Control</h2>
-          <h4>Experienced with Git and GitHub for version control. They manage code repositories, collaborate with teams, and maintain a history of code changes to streamline development workflows.</h4>
+          <h4>Skilled in Git and GitHub for version control, managing repositories, and tracking code changes.</h4>
         </div>
 
         <div className='education-container__info__list'>
           <h2>Design & Collaboration</h2>
-          <h4>Skilled in using Figma for design and Postman for API testing. They create design prototypes, collaborate on UI/UX, and ensure efficient API integration and testing for seamless application functionality.</h4>
+          <h4>Skilled in Figma for design and Postman for API testing, creating prototypes, collaborating on design, and ensuring efficient API integration.</h4>
         </div>
 
 
